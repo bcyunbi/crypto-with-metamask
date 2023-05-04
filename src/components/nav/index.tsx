a6021f4ef0b9fcd1af6e8bf74984bf510e36f567
Chloe Qu <chloe.qu@stranity.com>
2023-05-05 06:52:44 +0800

    feat: adjust logo

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,3 +1,4 @@
+import { LogoIcon } from '@/icon';
 import { useRouter } from 'next/router';
 const Navbar = () => {
     const router = useRouter();
@@ -9,7 +10,7 @@ const Navbar = () => {
         <div className=' bg-slate-50/50 text-white  p-4  h-16 flex items-center fixed top-0 left-0 w-full z-10 backdrop-blur-md'>
             <div className="w-full text-gray-700">
                 <div className="flex items-center gap-1 cursor-pointer" onClick={handleClick} >
-                    LOGO
+                    <div className='text-sky-400 text-sm'><LogoIcon /></div>
                 </div>
             </div>
         </div>
